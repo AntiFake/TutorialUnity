d4c9d543ee2a92d68e520da7119476f572b102f2
Alexeyyy <a.zhelepov@yandex.ru>
2015-06-27 22:17:53 +0300

    Working version

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    private BoardManager boardScript;                       //Store a reference to our BoardManager which will set up the level.
+    private int level = 3;                                  //Current level number, expressed in game as "Day 1".
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        boardScript = GetComponent<BoardManager>();
+        Debug.Log(GetComponent<BoardManager>() == null ? "1" : "2");
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+}
